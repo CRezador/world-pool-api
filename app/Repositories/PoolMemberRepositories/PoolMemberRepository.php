1870f647e0a2a1bcceb4c15dd07576d0213fc14a
CRezador <caio.rezador@gmail.com>
2026-04-18 00:49:14 -0300

    feat: refactor and add

--- app/Repositories/PoolMemberRepositories/PoolMemberRepository.php
+++ app/Repositories/PoolMemberRepositories/PoolMemberRepository.php
@@ -9,7 +9,7 @@ use App\Models\PoolMembers;
 
 class PoolMemberRepository
 {
-    public function isMember($poolId, $userId)
+    public function isMember(int $poolId, int $userId)
     {
         return PoolMembers::query()
             ->where('pool_id', $poolId)
@@ -17,19 +17,19 @@ class PoolMemberRepository
             ->exists();
     }
 
-    public function isAdmin($poolId, $userId)
+    public function isAdmin(int $poolId, int $userId): PoolMembers
     {
         return PoolMembers::query()
             ->where('pool_id', $poolId)
             ->where('user_id', $userId)
             ->where('role', PoolUserRole::ADMIN->value)
-            ->exists();
+            ->first();
     }
 
-    public function addMember($pool, $role, $userId)
+    public function addMember(int $poolId, string $role, int $userId): PoolMembers
     {
-        PoolMembers::create([
-            'pool_id' => $pool->id,
+        return PoolMembers::create([
+            'pool_id' => $poolId,
             'user_id' => $userId,
             'role' => $role,
             'status' => PoolMemberStatus::ACTIVE->value,
@@ -37,11 +37,21 @@ class PoolMemberRepository
         ]);
     }
 
-    public function getMembersByPoolId($poolId)
+    public function getMembersByPoolId(int $poolId): string
     {
         return PoolMembers::query()
             ->where('pool_id', $poolId)
             ->with('user:id,name') // Carrega os dados do usuário relacionado, selecionando apenas id e name
+            ->get();
+    }
+
+    public function getPoolsByUserId(int $userId)
+    {
+        //pegue todos os Pool onde o userId é membro em poolmembers e retorne os Pool relacionados
+        return Pool::query()
+            ->whereHas('members', function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })
             ->get();
     }
 }
--- app/Services/PoolMemberServices/PoolMemberService.php
+++ app/Services/PoolMemberServices/PoolMemberService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services\PoolMemberServices;
 
-use App\Http\Enums\PoolUserRole;
+use App\Models\PoolMembers;
 use App\Repositories\PoolMemberRepositories\PoolMemberRepository;
 use App\Repositories\PoolRepositories\PoolRepository;
 
@@ -10,48 +10,35 @@ class PoolMemberService
 {
     public function __construct(
         private PoolMemberRepository $poolMemberRepository,
-        private PoolRepository $poolRepository
     ) {
 
     }
 
-    public function joinPool($join_code, $user)
+
+
+    public function addMember(int $poolId, string $role, int $userId): PoolMembers
     {
-        $pool = $this->poolRepository->getPoolByJoinCode($join_code);
-
-        if (!$pool) {
-            throw new \Exception('Bolão não encontrado.');
-        }
-
-        // Verificar se o usuário já é membro do bolão
-        if ($this->poolMemberRepository->isMember($pool->id, $user->id)) {
-            throw new \Exception('Você já é membro deste bolão.');
-        }
-
-        // Adicionar o usuário como membro do bolão
-        $this->poolMemberRepository->addMember($pool->id, PoolUserRole::MEMBER->value, $user->id);
-
-        return $pool;
+        return $this->poolMemberRepository->addMember($poolId, $role, $userId);
     }
 
-    public function addMember($pool, $role, $userId)
-    {
-        $this->poolMemberRepository->addMember($pool, $role, $userId);
-    }
-
-    public function listMembers($poolId)
+    public function listMembers(int $poolId): string
     {
         return $this->poolMemberRepository->getMembersByPoolId($poolId);
     }
 
-    public function isMember($poolId, $userId)
+    public function isMember(int $poolId, int $userId): PoolMembers
     {
         return $this->poolMemberRepository->isMember($poolId, $userId);
     }
 
-    public function isAdmin($poolId, $userId)
+    public function isAdmin(int $poolId, int $userId)
     {
         return $this->poolMemberRepository->isAdmin($poolId, $userId);
+    }
+
+    public function getPoolsByUserId(int $userId)
+    {
+        return $this->poolMemberRepository->getPoolsByUserId($userId);
     }
 
 }
