1ffdc31d5403b869f65be3dffb6562c1d2aa6a6c
CRezador <caio.rezador@gmail.com>
2026-05-10 00:05:45 -0300

    feat(GuessRepository): Adicionando Repositories

--- api/app/Repositories/GuessRepositories/GuessRepository.php
+++ api/app/Repositories/GuessRepositories/GuessRepository.php
@@ -7,5 +7,61 @@ use Illuminate\Database\Eloquent\Collection;
 
 class GuessRepository
 {
-    //
+    public function create(array $data): Guess
+    {
+        return Guess::create($data);
+    }
+
+    public function findById(int $id): ?Guess
+    {
+        return Guess::find($id);
+    }
+
+    public function updateById(int $id, array $data): ?Guess
+    {
+        $guess = $this->findById($id);
+        if ($guess) {
+            $guess->update($data);
+        }
+
+        return $guess;
+    }
+
+    public function deleteById(int $id): bool
+    {
+        $guess = $this->findById($id);
+        if ($guess) {
+            $guess->delete();
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getByUserAndPool(int $userId, int $poolId): Collection
+    {
+        return Guess::where('user_id', $userId)
+            ->whereHas('match.groups.pools', function ($query) use ($poolId) {
+                $query->where('id', $poolId);
+            })
+            ->get();
+    }
+
+    public function getByMatchAndPool(int $matchId, int $poolId): Collection
+    {
+        return Guess::where('match_id', $matchId)
+            ->whereHas('match.groups.pools', function ($query) use ($poolId) {
+                $query->where('id', $poolId);
+            })
+            ->get();
+    }
+
+    public function getByMemberAndPool(int $memberId, int $poolId): Collection
+    {
+        return Guess::where('user_id', $memberId)
+            ->whereHas('match.groups.pools', function ($query) use ($poolId) {
+                $query->where('id', $poolId);
+            })
+            ->get();
+    }
 }
